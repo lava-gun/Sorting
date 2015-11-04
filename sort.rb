91c8e09fc4d87740889eef96dd716b4cc141b2eb
David Horne <david.horne@lyon.edu>
2015-11-04 12:07:24 -0600

    Finished the bubble sort

--- sort.rb
+++ sort.rb
@@ -1,20 +1,19 @@
 def bubble_sort(array)
-	swapped = true
-	until !swapped
-		for num in [1...array.size]
-			swapped = false
-			numm = num-1
-			if array[(numm)]>array[num]
-				temp=array[i]
-				array[i]=array[i+1]
-				array[i]=1
+	loop do 
+		swapped = false
+		i =0
+		while i< ((array.length) -1)
+			if array[i+1]<array[i]
+				array[i], array[i+1] = array[i+1], array[i]
 				swapped = true
 			end
+			i+=1
 		end
+		break if swapped == false
 	end
 	return array
 end
 
 puts "Enter a bunch of numbers to be sorted, separated by comments"
 numbers = gets.chomp.split(",")
-bubble_sort(numbers)
+print bubble_sort(numbers)
