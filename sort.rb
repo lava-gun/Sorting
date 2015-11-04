5af9605724f756ca50045d93e72a70f3e5b3251f
David Horne <david.horne@lyon.edu>
2015-11-04 12:33:29 -0600

    Finished up the project!

--- sort.rb
+++ sort.rb
@@ -17,3 +17,18 @@ end
 puts "Enter a bunch of numbers to be sorted, separated by comments"
 numbers = gets.chomp.split(",")
 print bubble_sort(numbers)
+
+def bubble_sort_by (array)
+	loop do 
+		swapped = false
+		i = 0
+		while i < (array.length -1)
+			if yield (array[i+1],array[i])>0
+				array[i], array[i+1] = array[i+1], array[i]
+				swapped = true
+			end
+			i+=1
+		end
+		beak if swapped == 0
+	end
+end
